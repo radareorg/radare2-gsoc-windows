9555a5bb4f3ecfc4f0918b9a5fe999d9da84fbf1
Alex Kornitzer <a.kornitzer@me.com>
2017-03-19 19:28:02 +0000

    Added in support for unions and defines

--- scripts/windbg_var_to_struct.py
+++ scripts/windbg_var_to_struct.py
@@ -6,6 +6,11 @@ Converts dumped variables from WinDbg that use the following command:
 Ideally this script should be used with the output files created by
     pykd_ntdll_variable_dumper.py
 
+Description:
+    Didn't want to use any external libs so everything remotely complex is
+    handled through the power of tuples. They are in the form:
+    (type, name, data) where 'type' and 'name' are strings and 'data' is an
+    array.
 '''
 
 
@@ -24,7 +29,7 @@ def convert_type(t):
         return 'uint32_t'
     if t == 'Uint8B':
         return 'uint64_t'
-    if t == 'Void':
+    if t == 'Void' or t == 'void':
         return 'void'
     if '[' in t and ']' in t:
         return t
@@ -39,61 +44,107 @@ def split_member(mbr):
     return offset, mbr_name, right
 
 
-def parse_struct(name, data):
+def parse_define_member(offset, mbr_name, raw_type):
+    pos, bit = raw_type.split(',')
+    pos = int(pos.split()[1])
+    bit = int(bit.split()[0])
+    return (('#define', mbr_name, int('1'*bit, 2)<<pos))
+
+
+def parse_struct_member(offset, mbr_name, raw_type):
+    raw_type = list(reversed(raw_type.split()))
+    n = mbr_name
+    t = ''
+    for elem in raw_type:
+        c_type = convert_type(elem)
+        if '[' in c_type and ']' in c_type:
+            n = n+c_type
+        elif '*' in c_type:
+            n = c_type+n
+        else:
+            t += c_type
+    return ((t, n, None))
+
+
+def parse_variable(name, data):
     mbrs = data.split('\n')[:-1]
-    struct = []
+
     defines = []
+    enums = []
+    structs = []
+
+    # Here we parse a struct, we have made an assumtion here!
     current_offset = 0
-    for i in range(0, len(mbrs)):
+    prev_name = ''
+    struct_mbrs = []
+    i = 0
+    while i < len(mbrs):
         mbr = mbrs[i]
         offset, mbr_name, raw_type = split_member(mbr)
-
         # If offsets match we hit a define, or union
-        if offset == current_offset and len(struct) > 0:
+        if offset == current_offset and i > 0:
+            # Define
             if 'Pos' in raw_type:
-                pos, bit = raw_type.split(',')
-                pos = int(pos.split()[1])
-                bit = int(bit.split()[0])
-                defines.append((mbr_name, int('1'*bit, 2)<<pos))
+                define_mbrs = []
+                for j in range(i, len(mbrs)):
+                    mbr = mbrs[j]
+                    offset, mbr_name, raw_type = split_member(mbr)
+                    if not offset == current_offset:
+                        break
+                    define_mbr = parse_define_member(offset, mbr_name, raw_type)
+                    define_mbrs.append(define_mbr)
+                defines.append((None, prev_name, define_mbrs))
+                i = j-1
+            # Unions
             else:
-                print('Found a union, eww')
+                union_mbrs = [struct_mbrs[-1]]
+                for j in range(i, len(mbrs)):
+                    mbr = mbrs[j]
+                    offset, mbr_name, raw_type = split_member(mbr)
+                    if not offset == current_offset:
+                        break
+                    union_mbr = parse_struct_member(offset, mbr_name, raw_type)
+                    union_mbrs.append(union_mbr)
+                struct_mbrs[-1] = (('union', None, union_mbrs))
+                i = j-1
         else:
-            # Work backwards :)
-            raw_type = list(reversed(raw_type.split()))
-            struct_mbr = []
-            for elem in raw_type:
-                c_type = convert_type(elem)
-                struct_mbr.append(c_type)
+            struct_mbr = parse_struct_member(offset, mbr_name, raw_type)
+            struct_mbrs.append(struct_mbr)
+            current_offset = offset
+            prev_name = mbr_name
+        i += 1
 
-            # Handle arrays
-            if '[' in struct_mbr[-1] and ']' in struct_mbr[-1]:
-                struct_mbr[-1] = mbr_name+struct_mbr[-1]
+    structs.append(('struct', name, struct_mbrs))
+
+    return {'defines' : defines, 'enums' : enums, 'structs' : structs}
+
+
+def format_data(data):
+    buf = ''
+    for define in data['defines']:
+        t, n, d = define
+        buf += '/* %s */\n' % (n)
+        for mbr in d:
+            t_mbr, n_mbr, d_mbr = mbr
+            buf += "%s\t%s\t0x%.08X\n" % (t_mbr, n_mbr, d_mbr)
+        buf += '\n'
+    for enum in data['enums']:
+        buf += '/* TODO: enums */\n'
+    for struct in data['structs']:
+        t, n, d = struct
+        buf += '%s %s {\n' % (t, n)
+        for mbr in d:
+            t_mbr, n_mbr, d_mbr = mbr
+            if t_mbr == 'union': # TODO: Recurse to handle nesting!
+                buf += "\tunion {\n"
+                for union in d_mbr:
+                    t_mbr, n_mbr, d_mbr = union
+                    buf += '\t\t%s\t%s;\n' % (t_mbr, n_mbr)
+                buf += "\t};\n"
             else:
-                struct_mbr.append(mbr_name)
-
-            struct.append(struct_mbr)
-
-        current_offset = offset
-    return ((name, struct), defines)
-
-
-# TODO: Remove and output to file
-def pretty_print(data):
-    name, struct = data
-    print('struct %s {' % name)
-    for member in struct:
-        line = ''
-        for elem in member:
-            line += '\t'+elem
-        print(line+';')
-    print('}__attribute__((packed));')
-
-
-def pretty_print_1(data):
-    for mbr in data:
-        name, value = mbr
-        print('#define\t%s\t0x%.08x' % (name, value))
-    print('')
+                buf += '\t%s\t%s;\n' % (t_mbr, n_mbr)
+        buf += '}__attribute__((packed));\n'
+    return buf
 
 
 def main():
@@ -113,14 +164,16 @@ def main():
 
     for f in os.listdir(input_dir):
         file_path = os.path.join(input_dir, f)
-        if os.path.isfile(file_path) and '.swp' not in file_path:
+        if os.path.isfile(file_path) and '.raw' in file_path:
             fp = open(file_path, 'r')
             data = fp.read()
             fp.close()
             name = f.split('!')[1].split('.')[0]
-            struct, defines = parse_struct(name, data)
-            pretty_print_1(defines)
-            pretty_print(struct)
+            data = parse_variable(name, data)
+            buf = format_data(data)
+            fp = open(os.path.join(output_dir, name+'.h'), 'w')
+            fp.write(buf)
+            fp.close()
 
 
 if __name__ == "__main__":
